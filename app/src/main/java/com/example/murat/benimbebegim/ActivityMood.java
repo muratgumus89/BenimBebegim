7b77cb226437f1a875215241fd0d2b94f1ecbb14
ayttunc <aytunc.matrac@hotmail.com.tr>
2015-04-01 00:55:37 +0300

    Activities still on process

--- app/src/main/java/com/example/murat/benimbebegim/ActivityMood.java
+++ app/src/main/java/com/example/murat/benimbebegim/ActivityMood.java
@@ -6,14 +6,25 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
-public class ActivityMood extends FragmentActivity {
+public class ActivityMood extends FragmentActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_mood);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spnr_MoodActivity_Moods);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.activity_mood_moods, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(ActivityMood.this);
     }
 
 
@@ -37,5 +48,15 @@ public class ActivityMood extends FragmentActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
