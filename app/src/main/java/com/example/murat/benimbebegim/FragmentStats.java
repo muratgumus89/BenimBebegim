f915c304ea336a3576dfd4d6f697e1efd470a8e2
ayttunc <aytunc.matrac@hotmail.com.tr>
2015-04-11 23:23:55 +0300

    Chart Library added

--- app/src/main/java/com/example/murat/benimbebegim/FragmentStats.java
+++ app/src/main/java/com/example/murat/benimbebegim/FragmentStats.java
@@ -2,14 +2,38 @@ package com.example.murat.benimbebegim;
 
 
 import android.app.ActionBar;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.Highlight;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class FragmentStats extends Fragment {
+public class FragmentStats extends Fragment implements OnChartValueSelectedListener {
+
     public static final String TAG = FragmentStats.class.getSimpleName();
     private ActionBar actionBar;
     public static FragmentStats newInstance() {
@@ -24,6 +48,18 @@ public class FragmentStats extends Fragment {
 		View view = inflater.inflate(R.layout.layout_statsfragment, container,false);
         actionBar=getActivity().getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-		return view;
+
+
+        return view;
 	}
+
+    @Override
+    public void onValueSelected(Entry entry, int i, Highlight highlight) {
+
+    }
+
+    @Override
+    public void onNothingSelected() {
+
+    }
 }
