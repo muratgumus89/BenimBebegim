c5bdb1140b9cb8c9afdaf3bba2c21fa7e2ca28d7
ayttunc <aytunc.matrac@hotmail.com.tr>
2015-04-26 02:29:32 +0300

    activity breast redesigned

--- app/src/main/java/com/example/murat/benimbebegim/ActivityBreast.java
+++ app/src/main/java/com/example/murat/benimbebegim/ActivityBreast.java
@@ -44,21 +44,36 @@ public class ActivityBreast extends FragmentActivity implements View.OnClickList
     String selectedDate, selectedTime, strTime, strDate, getBabyName, getUserId,
             selectedGendersForCreateBaby, strSelectedImage = "null";
 
-    private TextView tempTextView; //Temporary TextView
-    private Button tempBtn; //Temporary Button
     private Handler mHandler = new Handler();
     private long startTime;
     private long elapsedTime;
     private final int REFRESH_RATE = 100;
     private String hours, minutes, seconds, milliseconds;
-    private long secs, mins, hrs, msecs;
+    private long secs, mins, hrs;
     private boolean stopped = false;
     boolean stateLeft = true; // T: duruyor F: calısıyor
+
+    private Handler mHandler1 = new Handler();
+    private long startTime1;
+    private long elapsedTime1;
+    private final int REFRESH_RATE1 = 100;
+    private String hours1, minutes1, seconds1, milliseconds1;
+    private long secs1, mins1, hrs1;
+    private boolean stopped1 = false;
+    boolean stateLeft1 = true; // T: duruyor F: calısıyor
+
     private Runnable startTimer = new Runnable() {
         public void run() {
             elapsedTime = System.currentTimeMillis() - startTime;
             updateTimer(elapsedTime);
             mHandler.postDelayed(this, REFRESH_RATE);
+        }
+    };
+    private Runnable startTimer1 = new Runnable() {
+        public void run() {
+            elapsedTime1 = System.currentTimeMillis() - startTime1;
+            updateTimer1(elapsedTime1);
+            mHandler1.postDelayed(this, REFRESH_RATE1);
         }
     };
 
@@ -146,7 +161,83 @@ public class ActivityBreast extends FragmentActivity implements View.OnClickList
         if (milliseconds.length() > 3 && milliseconds.length() < 4) {
             milliseconds = milliseconds.substring(milliseconds.length() - 3, milliseconds.length() - 2);
         }
-        ((Button) findViewById(R.id.btnBottleActivity_time)).setText(hours + " : " + minutes + " : " + seconds);
+        ((Button) findViewById(R.id.btnBreastActivity_LeftBreast)).setText(hours + " : " + minutes + " : " + seconds);
+    }
+
+    public void startClicks1() {
+        if (stopped1) {
+            startTime1 = System.currentTimeMillis() - elapsedTime1;
+        } else {
+            startTime1 = System.currentTimeMillis();
+        }
+        mHandler1.removeCallbacks(startTimer1);
+        mHandler1.postDelayed(startTimer1, 0);
+    }
+
+    public void stopClicks1() {
+        mHandler1.removeCallbacks(startTimer1);
+        stopped1 = true;
+    }
+
+    private void updateTimer1(float time) {
+        secs1 = (long) (time / 1000);
+        mins1 = (long) ((time / 1000) / 60);
+        hrs1 = (long) (((time / 1000) / 60) / 60);
+
+		/* Convert the seconds to String
+         * and format to ensure it has
+		 * a leading zero when required
+		 */
+        secs1 = secs1 % 60;
+        seconds1 = String.valueOf(secs1);
+        if (secs1 == 0) {
+            seconds1 = "00";
+        }
+        if (secs1 < 10 && secs1 > 0) {
+            seconds1 = "0" + seconds1;
+        }
+
+		/* Convert the minutes to String and format the String */
+
+        mins1 = mins1 % 60;
+        minutes1 = String.valueOf(mins1);
+        if (mins1 == 0) {
+            minutes1 = "00";
+        }
+        if (mins1 < 10 && mins1 > 0) {
+            minutes1 = "0" + minutes1;
+        }
+
+    	/* Convert the hours to String and format the String */
+
+        hours1 = String.valueOf(hrs1);
+        if (hrs1 == 0) {
+            hours1 = "00";
+        }
+        if (hrs1 < 10 && hrs1 > 0) {
+            hours1 = "0" + hours1;
+        }
+
+    	/* Although we are not using milliseconds on the timer in this example
+    	 * I included the code in the event that you wanted to include it on your own
+    	 */
+        milliseconds1 = String.valueOf((long) time);
+        Log.i("miliseconds", String.valueOf(milliseconds1.length()) + " ::" + milliseconds1);
+
+
+        if (milliseconds1.length() == 2) {
+            milliseconds1 = "0" + milliseconds1;
+        }
+        if (milliseconds1.length() <= 1) {
+            milliseconds1 = "00";
+        }
+        if (milliseconds1.length() >= 3) {
+            milliseconds1 = milliseconds1.substring(milliseconds1.length() - 3, milliseconds1.length() - 2);
+        }
+        if (milliseconds1.length() > 3 && milliseconds1.length() < 4) {
+            milliseconds1 = milliseconds1.substring(milliseconds1.length() - 3, milliseconds1.length() - 2);
+        }
+        ((Button) findViewById(R.id.btnBreastActivity_RightBreast)).setText(hours1 + " : " + minutes1 + " : " + seconds1);
     }
 
     private void init(){
@@ -156,12 +247,14 @@ public class ActivityBreast extends FragmentActivity implements View.OnClickList
         btnSave = (Button)findViewById(R.id.btnBreastActivity_Save);
         btnLeftBreast = (Button)findViewById(R.id.btnBreastActivity_LeftBreast);
         btnRightBreast = (Button)findViewById(R.id.btnBreastActivity_RightBreast);
-        btnSave = (Button)findViewById(R.id.btnBreastActivity_Save);
+        btnLeftBreast = (Button)findViewById(R.id.btnBreastActivity_LeftBreast);
 
         btnDatePicker.setOnClickListener(this);
         btnTimePicker.setOnClickListener(this);
         btnCancel.setOnClickListener(this);
         btnSave.setOnClickListener(this);
+        btnRightBreast.setOnClickListener(this);
+        btnLeftBreast.setOnClickListener(this);
 
         dateForDB = new DatePickerDialog.OnDateSetListener() {
 
@@ -240,21 +333,21 @@ public class ActivityBreast extends FragmentActivity implements View.OnClickList
                 Log.i("IS IT WORKING", "yes");
                 break;
             case R.id.btnBreastActivity_LeftBreast:
-                if (state == true) {
+                if (stateLeft == true) {
                     startClicks();
-                    state = false;
+                    stateLeft = false;
                 } else {
                     stopClicks();
-                    state = true;
+                    stateLeft = true;
                 }
                 break;
-            case R.id.btnPumpingActivity_RightBreast:
-                if (state == true) {
-                    startClicks();
-                    state = false;
+            case R.id.btnBreastActivity_RightBreast:
+                if (stateLeft1 == true) {
+                    startClicks1();
+                    stateLeft1 = false;
                 } else {
-                    stopClicks();
-                    state = true;
+                    stopClicks1();
+                    stateLeft1 = true;
                 }
                 break;
             default:
