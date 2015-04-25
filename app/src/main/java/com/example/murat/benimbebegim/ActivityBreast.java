903d23a940ace207a71b54a1aa428981ec51bfd1
ayttunc <aytunc.matrac@hotmail.com.tr>
2015-04-26 01:23:42 +0300

    more desing working

--- app/src/main/java/com/example/murat/benimbebegim/ActivityBreast.java
+++ app/src/main/java/com/example/murat/benimbebegim/ActivityBreast.java
@@ -20,15 +20,21 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.TimePicker;
+import android.os.Handler;
 
 import java.util.Calendar;
 
 
 public class ActivityBreast extends FragmentActivity implements View.OnClickListener {
 
-    Button btnSave, btnCancel, btnDatePicker, btnTimePicker;
+    /**********************************************************************************************/
+    /**********************************************************************************************/
+    /**********************************************************************************************/
+    Button btnSave, btnCancel, btnDatePicker, btnTimePicker, btnLeftBreast, btnRightBreast;
     EditText etNote;
+    TextView txtTotal, txtPaused, txtActual;
 
     DatePickerDialog.OnDateSetListener dateForDB;
     TimePickerDialog.OnTimeSetListener timeForDB;
@@ -38,6 +44,28 @@ public class ActivityBreast extends FragmentActivity implements View.OnClickList
     String selectedDate, selectedTime, strTime, strDate, getBabyName, getUserId,
             selectedGendersForCreateBaby, strSelectedImage = "null";
 
+    private TextView tempTextView; //Temporary TextView
+    private Button tempBtn; //Temporary Button
+    private Handler mHandler = new Handler();
+    private long startTime;
+    private long elapsedTime;
+    private final int REFRESH_RATE = 100;
+    private String hours, minutes, seconds, milliseconds;
+    private long secs, mins, hrs, msecs;
+    private boolean stopped = false;
+    boolean stateLeft = true; // T: duruyor F: calısıyor
+    private Runnable startTimer = new Runnable() {
+        public void run() {
+            elapsedTime = System.currentTimeMillis() - startTime;
+            updateTimer(elapsedTime);
+            mHandler.postDelayed(this, REFRESH_RATE);
+        }
+    };
+
+    /**********************************************************************************************/
+    /**********************************************************************************************/
+    /**********************************************************************************************/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,10 +73,89 @@ public class ActivityBreast extends FragmentActivity implements View.OnClickList
         init();
     }
 
+    public void startClicks() {
+        if (stopped) {
+            startTime = System.currentTimeMillis() - elapsedTime;
+        } else {
+            startTime = System.currentTimeMillis();
+        }
+        mHandler.removeCallbacks(startTimer);
+        mHandler.postDelayed(startTimer, 0);
+    }
+
+    public void stopClicks() {
+        mHandler.removeCallbacks(startTimer);
+        stopped = true;
+    }
+
+    private void updateTimer(float time) {
+        secs = (long) (time / 1000);
+        mins = (long) ((time / 1000) / 60);
+        hrs = (long) (((time / 1000) / 60) / 60);
+
+		/* Convert the seconds to String
+         * and format to ensure it has
+		 * a leading zero when required
+		 */
+        secs = secs % 60;
+        seconds = String.valueOf(secs);
+        if (secs == 0) {
+            seconds = "00";
+        }
+        if (secs < 10 && secs > 0) {
+            seconds = "0" + seconds;
+        }
+
+		/* Convert the minutes to String and format the String */
+
+        mins = mins % 60;
+        minutes = String.valueOf(mins);
+        if (mins == 0) {
+            minutes = "00";
+        }
+        if (mins < 10 && mins > 0) {
+            minutes = "0" + minutes;
+        }
+
+    	/* Convert the hours to String and format the String */
+
+        hours = String.valueOf(hrs);
+        if (hrs == 0) {
+            hours = "00";
+        }
+        if (hrs < 10 && hrs > 0) {
+            hours = "0" + hours;
+        }
+
+    	/* Although we are not using milliseconds on the timer in this example
+    	 * I included the code in the event that you wanted to include it on your own
+    	 */
+        milliseconds = String.valueOf((long) time);
+        Log.i("miliseconds", String.valueOf(milliseconds.length()) + " ::" + milliseconds);
+
+
+        if (milliseconds.length() == 2) {
+            milliseconds = "0" + milliseconds;
+        }
+        if (milliseconds.length() <= 1) {
+            milliseconds = "00";
+        }
+        if (milliseconds.length() >= 3) {
+            milliseconds = milliseconds.substring(milliseconds.length() - 3, milliseconds.length() - 2);
+        }
+        if (milliseconds.length() > 3 && milliseconds.length() < 4) {
+            milliseconds = milliseconds.substring(milliseconds.length() - 3, milliseconds.length() - 2);
+        }
+        ((Button) findViewById(R.id.btnBottleActivity_time)).setText(hours + " : " + minutes + " : " + seconds);
+    }
+
     private void init(){
         btnDatePicker = (Button)findViewById(R.id.btnBreastActivity_Date);
         btnTimePicker = (Button)findViewById(R.id.btnBreastActivity_Time);
         btnCancel = (Button)findViewById(R.id.btnBreastActivity_Cancel);
+        btnSave = (Button)findViewById(R.id.btnBreastActivity_Save);
+        btnLeftBreast = (Button)findViewById(R.id.btnBreastActivity_LeftBreast);
+        btnRightBreast = (Button)findViewById(R.id.btnBreastActivity_RightBreast);
         btnSave = (Button)findViewById(R.id.btnBreastActivity_Save);
 
         btnDatePicker.setOnClickListener(this);
@@ -131,6 +238,24 @@ public class ActivityBreast extends FragmentActivity implements View.OnClickList
                 break;
             case R.id.btnBreastActivity_Save:
                 Log.i("IS IT WORKING", "yes");
+                break;
+            case R.id.btnBreastActivity_LeftBreast:
+                if (state == true) {
+                    startClicks();
+                    state = false;
+                } else {
+                    stopClicks();
+                    state = true;
+                }
+                break;
+            case R.id.btnPumpingActivity_RightBreast:
+                if (state == true) {
+                    startClicks();
+                    state = false;
+                } else {
+                    stopClicks();
+                    state = true;
+                }
                 break;
             default:
                 break;
--- app/src/main/java/com/example/murat/benimbebegim/ActivityPumping.java
+++ app/src/main/java/com/example/murat/benimbebegim/ActivityPumping.java
@@ -16,14 +16,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
+import android.os.Handler;
 
 import java.util.Calendar;
 
 
 public class ActivityPumping extends FragmentActivity implements View.OnClickListener {
 
-    Button btnSave, btnCancel, btnDatePicker, btnTimePicker;
+
+    /*******************************************************************************************/
+    /*******************************************************************************************/
+    /**
+     * ***************************************************************************************
+     */
+    Button btnSave, btnCancel, btnDatePicker, btnTimePicker, btnLeftBreast, btnRightBreast;
     EditText etNote;
 
     DatePickerDialog.OnDateSetListener dateForDB;
@@ -34,6 +42,29 @@ public class ActivityPumping extends FragmentActivity implements View.OnClickLis
     String selectedDate, selectedTime, strTime, strDate, getBabyName, getUserId,
             selectedGendersForCreateBaby, strSelectedImage = "null";
 
+    private TextView tempTextView; //Temporary TextView
+    private Button tempBtn; //Temporary Button
+    private Handler mHandler = new Handler();
+    private long startTime;
+    private long elapsedTime;
+    private final int REFRESH_RATE = 100;
+    private String hours, minutes, seconds, milliseconds;
+    private long secs, mins, hrs, msecs;
+    private boolean stopped = false;
+    boolean state = true; // T: duruyor F: calısıyor
+    private Runnable startTimer = new Runnable() {
+        public void run() {
+            elapsedTime = System.currentTimeMillis() - startTime;
+            updateTimer(elapsedTime);
+            mHandler.postDelayed(this, REFRESH_RATE);
+        }
+    };
+    /*******************************************************************************************/
+    /*******************************************************************************************/
+    /**
+     * ***************************************************************************************
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,16 +72,98 @@ public class ActivityPumping extends FragmentActivity implements View.OnClickLis
         init();
     }
 
+
+    public void startClicks() {
+        if (stopped) {
+            startTime = System.currentTimeMillis() - elapsedTime;
+        } else {
+            startTime = System.currentTimeMillis();
+        }
+        mHandler.removeCallbacks(startTimer);
+        mHandler.postDelayed(startTimer, 0);
+    }
+
+    public void stopClicks() {
+        mHandler.removeCallbacks(startTimer);
+        stopped = true;
+    }
+
+    private void updateTimer(float time) {
+        secs = (long) (time / 1000);
+        mins = (long) ((time / 1000) / 60);
+        hrs = (long) (((time / 1000) / 60) / 60);
+
+		/* Convert the seconds to String
+         * and format to ensure it has
+		 * a leading zero when required
+		 */
+        secs = secs % 60;
+        seconds = String.valueOf(secs);
+        if (secs == 0) {
+            seconds = "00";
+        }
+        if (secs < 10 && secs > 0) {
+            seconds = "0" + seconds;
+        }
+
+		/* Convert the minutes to String and format the String */
+
+        mins = mins % 60;
+        minutes = String.valueOf(mins);
+        if (mins == 0) {
+            minutes = "00";
+        }
+        if (mins < 10 && mins > 0) {
+            minutes = "0" + minutes;
+        }
+
+    	/* Convert the hours to String and format the String */
+
+        hours = String.valueOf(hrs);
+        if (hrs == 0) {
+            hours = "00";
+        }
+        if (hrs < 10 && hrs > 0) {
+            hours = "0" + hours;
+        }
+
+    	/* Although we are not using milliseconds on the timer in this example
+    	 * I included the code in the event that you wanted to include it on your own
+    	 */
+        milliseconds = String.valueOf((long) time);
+        Log.i("miliseconds", String.valueOf(milliseconds.length()) + " ::" + milliseconds);
+
+
+        if (milliseconds.length() == 2) {
+            milliseconds = "0" + milliseconds;
+        }
+        if (milliseconds.length() <= 1) {
+            milliseconds = "00";
+        }
+        if (milliseconds.length() >= 3) {
+            milliseconds = milliseconds.substring(milliseconds.length() - 3, milliseconds.length() - 2);
+        }
+        if (milliseconds.length() > 3 && milliseconds.length() < 4) {
+            milliseconds = milliseconds.substring(milliseconds.length() - 3, milliseconds.length() - 2);
+        }
+        ((Button) findViewById(R.id.btnPumpingActivity_LeftBreast)).setText(minutes + ":" + seconds + ":" + milliseconds);
+    }
+
+
     private void init() {
         btnDatePicker = (Button) findViewById(R.id.btnPumpingActivity_Date);
         btnTimePicker = (Button) findViewById(R.id.btnPumpingActivity_Time);
         btnCancel = (Button) findViewById(R.id.btnPumpingActivity_Cancel);
         btnSave = (Button) findViewById(R.id.btnPumpingActivity_Save);
+        btnLeftBreast = (Button) findViewById(R.id.btnPumpingActivity_LeftBreast);
+        btnRightBreast = (Button) findViewById(R.id.btnPumpingActivity_RightBreast);
 
         btnDatePicker.setOnClickListener(this);
         btnTimePicker.setOnClickListener(this);
         btnCancel.setOnClickListener(this);
         btnSave.setOnClickListener(this);
+        btnLeftBreast.setOnClickListener(this);
+        btnRightBreast.setOnClickListener(this);
 
         dateForDB = new DatePickerDialog.OnDateSetListener() {
 
@@ -127,6 +240,18 @@ public class ActivityPumping extends FragmentActivity implements View.OnClickLis
                 break;
             case R.id.btnPumpingActivity_Save:
                 Log.i("IS IT WORKING", "yes");
+                break;
+            case R.id.btnPumpingActivity_LeftBreast:
+                if (state == true) {
+                    startClicks();
+                    state = false;
+                } else {
+                    stopClicks();
+                    state = true;
+                }
+                break;
+            case R.id.btnPumpingActivity_RightBreast:
+
                 break;
             default:
                 break;
